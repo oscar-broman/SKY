712f87289f50dcd9e4d6b8259225c697a473bfd5
Oscar Broman <oscar.broman@gmail.com>
2015-01-28 22:18:53 +0100

    fix sign on analog key values

--- src/Structs.h
+++ src/Structs.h
@@ -178,8 +178,8 @@ public:
 class CSyncData
 {
 	public:
-		WORD			wUDAnalog;				// 0x0076 - 0x0078
-		WORD			wLRAnalog;				// 0x0078 - 0x007A
+		signed short		wUDAnalog;				// 0x0076 - 0x0078
+		signed short		wLRAnalog;				// 0x0078 - 0x007A
 		WORD			wKeys;					// 0x007A - 0x007C
 		CVector			vecPosition;			// 0x007C - 0x0088
 		float			fQuaternionAngle;		// 0x0088 - 0x008C
