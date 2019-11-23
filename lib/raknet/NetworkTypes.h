6469aaacf2922a3667b9c8afd59f00bc8c537f23
Arron Franklin <adrfranklin95@gmail.com>
2019-11-23 03:19:36 +0000

    Updated the raknet library to match YSF

--- lib/raknet/NetworkTypes.h
+++ lib/raknet/NetworkTypes.h
@@ -21,16 +21,16 @@
 /// Forward declaration
 namespace RakNet
 {
-	class BitStream;
+class BitStream;
 };
 
 /// These enumerations are used to describe when packets are delivered.
 enum PacketPriority
 {
-	SYSTEM_PRIORITY,   /// \internal Used by RakNet to send above-high priority messages.
+	SYSTEM_PRIORITY, /// \internal Used by RakNet to send above-high priority messages.
 	HIGH_PRIORITY,   /// High priority messages are send before medium priority messages.
-	MEDIUM_PRIORITY,   /// Medium priority messages are send before low priority messages.
-	LOW_PRIORITY,   /// Low priority messages are only sent when no other messages are waiting.
+	MEDIUM_PRIORITY, /// Medium priority messages are send before low priority messages.
+	LOW_PRIORITY,	/// Low priority messages are only sent when no other messages are waiting.
 	NUMBER_OF_PRIORITIES
 };
 
@@ -38,17 +38,17 @@ enum PacketPriority
 /// \note  Note to self: I write this with 3 bits in the stream.  If I add more remember to change that
 enum PacketReliability
 {
-	UNRELIABLE = 6,   /// Same as regular UDP, except that it will also discard duplicate datagrams.  RakNet adds (6 to 17) + 21 bits of overhead, 16 of which is used to detect duplicate packets and 6 to 17 of which is used for message length.
-	UNRELIABLE_SEQUENCED,  /// Regular UDP with a sequence counter.  Out of order messages will be discarded.  This adds an additional 13 bits on top what is used for UNRELIABLE.
-	RELIABLE,   /// The message is sent reliably, but not necessarily in any order.  Same overhead as UNRELIABLE.
-	RELIABLE_ORDERED,   /// This message is reliable and will arrive in the order you sent it.  Messages will be delayed while waiting for out of order messages.  Same overhead as UNRELIABLE_SEQUENCED.
-	RELIABLE_SEQUENCED /// This message is reliable and will arrive in the sequence you sent it.  Out or order messages will be dropped.  Same overhead as UNRELIABLE_SEQUENCED.
+	UNRELIABLE = 6,		  /// Same as regular UDP, except that it will also discard duplicate datagrams.  RakNet adds (6 to 17) + 21 bits of overhead, 16 of which is used to detect duplicate packets and 6 to 17 of which is used for message length.
+	UNRELIABLE_SEQUENCED, /// Regular UDP with a sequence counter.  Out of order messages will be discarded.  This adds an additional 13 bits on top what is used for UNRELIABLE.
+	RELIABLE,			  /// The message is sent reliably, but not necessarily in any order.  Same overhead as UNRELIABLE.
+	RELIABLE_ORDERED,	 /// This message is reliable and will arrive in the order you sent it.  Messages will be delayed while waiting for out of order messages.  Same overhead as UNRELIABLE_SEQUENCED.
+	RELIABLE_SEQUENCED	/// This message is reliable and will arrive in the sequence you sent it.  Out or order messages will be dropped.  Same overhead as UNRELIABLE_SEQUENCED.
 };
 
 struct RPCParameters;
 
-#define BITS_TO_BYTES(x) (((x)+7)>>3)
-#define BYTES_TO_BITS(x) ((x)<<3)
+#define BITS_TO_BYTES(x) (((x) + 7) >> 3)
+#define BYTES_TO_BITS(x) ((x) << 3)
 
 /// First byte of a network message
 typedef unsigned char MessageID;
@@ -59,7 +59,7 @@ typedef unsigned short PlayerIndex;
 typedef unsigned char RPCIndex;
 const int MAX_RPC_MAP_SIZE = ((RPCIndex)-1) - 1;
 
-using RPCFunction = void(*)(RPCParameters *p);
+using RPCFunction = void (*)(RPCParameters *p);
 
 #pragma pack(push, 1)
 
@@ -70,9 +70,8 @@ struct PlayerID
 };
 
 const PlayerID UNASSIGNED_PLAYER_ID =
-{
-	0xFFFFFFFF, 0xFFFF
-};
+	{
+		0xFFFFFFFF, 0xFFFF};
 
 struct Packet
 {
@@ -80,7 +79,7 @@ struct Packet
 	PlayerID playerId;
 	unsigned int length;
 	unsigned int bitSize;
-	unsigned char* data;
+	unsigned char *data;
 	bool deleteData;
 };
 
@@ -90,5 +89,7 @@ struct RPCParameters
 	unsigned int numberOfBitsOfData;
 	PlayerID sender;
 };
+
+#pragma pack(pop)
 
 #endif
